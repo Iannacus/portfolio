a401a8b90a3a2fa012f91e0ed45abc20231b6df4
Iannacus <ian.rosasmal@gmail.com>
2021-04-09 14:12:11 -0500

    arregla menu desplegable

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,12 @@ let counter = 0;
 
 menuDesplegable.addEventListener("click", e => {
 
-    if(showMenu.style.display === "none") {showMenu.style.display = "block"}
-    //  if(counter%2 === 0){
-    //      e.target.parentElement.nextElementSibling.style.display = "block";
-    //      counter++;
-    //  }else {
-    //      e.target.parentElement.nextElementSibling.style.display = "none";
-    //      counter++;
-    //  }
+      if(counter%2 === 0){
+          e.target.parentElement.nextElementSibling.style.display = "block";
+          counter++;
+      }else {
+          e.target.parentElement.nextElementSibling.style.display = "none";
+         counter++;
+    }
     
 });
