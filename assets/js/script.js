6f2cd95a3df93ad9305e5b72ee85726a34b51bb4
Iannacus <ian.rosasmal@gmail.com>
2021-04-09 14:06:49 -0500

    se renombran clases para seguir un lineamiento

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
-const menuDesplegable = document.querySelector(".lista").firstElementChild.firstElementChild;
+const menuDesplegable = document.getElementById("menu").firstElementChild;
+const showMenu = document.getElementById("showMenu");
 let counter = 0;
 
+
+
 menuDesplegable.addEventListener("click", e => {
-    if(counter%2 === 0){
-        e.target.parentElement.nextElementSibling.style.display = "block";
-    }else {
-        e.target.parentElement.nextElementSibling.style.display = "none";
-    }
-    counter++;
+
+    if(showMenu.style.display === "none") {showMenu.style.display = "block"}
+    //  if(counter%2 === 0){
+    //      e.target.parentElement.nextElementSibling.style.display = "block";
+    //      counter++;
+    //  }else {
+    //      e.target.parentElement.nextElementSibling.style.display = "none";
+    //      counter++;
+    //  }
+    
 });
